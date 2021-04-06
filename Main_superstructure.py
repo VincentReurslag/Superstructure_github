4adbbdc8ac69379c8b88054d248ba53ef5094512
VincentReurslag <76790381+VincentReurslag@users.noreply.github.com>
2021-04-06 10:17:16 +0200

    Mainly comments

--- Main_superstructure.py
+++ Main_superstructure.py
@@ -4,12 +4,14 @@ Created on Sat Apr  3 15:21:03 2021
 
 @author: Vincent Reurslag
 
+Run this script to execute te entire program
+
 """
-import pandas as pd
 from Superstructure_class import Superstructure
 from Superstructure_model import Superstructure_model
 from Excel_write import excel_write
 
+#xlsx_file is the input file with the data, output_file will be the result file
 xlsx_file = 'SS_dataV2.xlsx'
 output_file = 'ResultsV2.xlsx'
 
--- Superstructure_model.py
+++ Superstructure_model.py
@@ -8,16 +8,16 @@ Created on Sun Mar 28 16:32:52 2021
 from pyomo.environ import *
 
 
-
 def Superstructure_model(Superstructure):
     model = ConcreteModel()
     
-    #initialize sets
+    #initialize sets, want to add utilities u later to this
     model.j = Set(initialize = Superstructure.j, doc = 'process stages')
     model.k = Set(initialize = Superstructure.k, doc = 'Technology options')
     model.i = Set(initialize = Superstructure.i, doc = 'Species in reaction mixture')
     
-    #initialize parameters
+    #initialize parameters. The data comes from the created Superstructure class
+    #M parameter is for the big M notation later in the model
     model.SF = Param(model.j, model.k, model.i, initialize = Superstructure.SF_data, doc = 'Split factor data for equipment types')
     model.EC = Param(model.j, model.k, initialize = Superstructure.EC_data, doc = 'Cost data for equipment types')
     model.flow0 = Param(model.i, initialize = Superstructure.F0_data, doc = 'Flow coming out of membrane reactor')
@@ -32,7 +32,9 @@ def Superstructure_model(Superstructure):
     model.flowstage = Var(model.j, model.i, bounds = (0.0, None))
     
     def massbalance_rule1(model, j, k, i):
-        """3 massbalance rules for big M implementation"""
+        """3 massbalance rules for big M implementation:
+            It calculates flow[j,k,i] = flow at previous stage * Separation factor * logic_variable
+            This equation is an MINLP so it is converted using the big M notation for performance"""
         if j >= 2:
             return model.flow[j,k,i] <= model.flowstage[j-1,i] * model.SF[j,k,i] + model.M * (1-model.y[j,k])
         else:
@@ -55,33 +57,40 @@ def Superstructure_model(Superstructure):
     model.massrule3 = Constraint(model.j, model.k, model.i, rule = massbalance_rule3)
     
     def flowstage_rule(model, j, i):
+        """Simply calculating the total flow at a stage by summing for every equipment at that stage"""
         return model.flowstage[j,i] == sum(model.flow[j,k,i] for k in model.k)
     
     model.flowstage_rule = Constraint(model.j, model.i, rule = flowstage_rule)
     
     def logic_rule(model, j):
+        """Only 1 equipment can be chosen at a stage"""
         return sum(model.y[j,k] for k in model.k) == 1
     
     model.logic = Constraint(model.j, rule = logic_rule, doc = 'One option per stage')
     
     def BDP_rule(model):
+        """Determine how much  biodiesel is produced at the final stage"""
         return model.BDP == model.flow[5,1,1]
     
     model.BDP_rule = Constraint(rule = BDP_rule, doc = 'Biodiesel produced')
     
     def flowtot_rule(model, j, k):
+        """Sum up the total flow for every component for economy of scale calculation"""
         return model.flowtot[j,k] == sum(model.flow[j,k,i] for i in model.i) 
     
     model.flowtot_rule = Constraint(model.j, model.k, rule = flowtot_rule)
     
     def TEC_rule(model):
+        """For every k and every j multiply the equipment cost with the total flow and sum it up to determine TEC"""
         return model.TEC == sum(model.EC[j,k] * model.flowtot[j,k] for j in model.j for k in model.k)
     
     model.TEC_rule = Constraint(rule = TEC_rule)
     
     def objective_rule(model):
+        """Objective is to minimize cost (TEC)"""
         return model.TEC
     
+    #Minimize the TEC
     model.objective = Objective(rule = objective_rule, sense = minimize)
     
     
